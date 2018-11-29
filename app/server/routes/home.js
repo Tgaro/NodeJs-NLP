32bc4b5b1c625c3bb964e918a2a806d662664504
tgaro <tiago_oa@hotmail.com>
2018-11-29 15:03:49 -0200

    Changed login logic as I have included authentication logic to the project

--- app/server/routes/home.js
+++ app/server/routes/home.js
@@ -1,19 +1,6 @@
 module.exports = app => {
 
-	app.post('/home', (req, res) => {
-
-		req.assert('user', 'You must enter a username').notEmpty()
-		req.assert('pwd', 'You must enter a password').notEmpty()
-		
-		console.log(req.params)
-		console.log(req.body)
-
-		req.validationErrors()
-		? res.status(400).send(`Please check your url. It seems that you haven't logged yet.`)
-		: res.render('home', {
-
-			user: req.body.user
-		})
-
+	app.get('/home', (req, res) => {
+		res.render('home', {user: ''})
 	})
 }
--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const consign = require('consign')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
+const expressSession = require('express-session')
 const app = express()
 
 app.set('views', './app/client/views')
@@ -11,10 +12,16 @@ app.use(express.static('./app/client/public'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(expressValidator())
+app.use(expressSession({
+	secret: 'chatbotgo', // segredo para gravar o coockie
+	resave: false,
+	saveUninitialized: false
+}))
 
 consign()
 	.include('./app/server/controller')
 	.then('./app/server/routes')
+	.then('./app/server/models')
 	.into(app)
 
 module.exports = app
